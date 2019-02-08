0af0c63bd54696f31240be66d7502e7739fde219
quang nguyen <quangnguyen@quangs-MacBook-Pro.local>
2019-02-08 13:30:58 -0500

    add parser

---
+++ our_csv_parser.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+
+myfilename = "housing.data.txt"
+
+# if os.path.isfile(myfilename):
+#   print("yay, I have a file")
+#   if sky == blue:
+#     print('yay the sky is blue')
+# else:
+#   print ('boo, no files for me')
+new_lists = []
+with open(myfilename, 'r') as file_handle:
+    for line in file_handle.readlines():
+        line_clean = line.replace('   ', ' ').replace('  ', ' ')
+        line_clean = line_clean.strip()
+        values = line_clean.split(' ')
+        # print(len(values))
+        # print(values)
+        casted_lines = []
+        for value in values:
+            # for homework:
+            # print('the type of {0} is {1} '.format(value, type(value)))
+            if '.' not in value:
+                value = int(value)
+            else:
+                value = float(value)
+            casted_lines.append(value)
+            # identify what type of data each value is, and cast it
+            # to the appropriate type, then print the new, properly-typed
+            # list to the screen.
+            # I.e. ['0.04741', '0.00', '11.930', '0', '0.5730', '6.0300', '80.80', '2.5050', '1', '273.0', '21.00', '396.90', '7.88', '11.90']
+            # becomes: [0.04741, 0.0, 11.93, 0, 0.573, '6.03, 80.8, 2.505, 1, 273.0, 21.0, 396.90, 7.88, 11.90]
+            # print(float(value))
+        # print(values)
+        print(casted_lines)
+
+   # part 2 of homework
+        new_lists.append(casted_lines)
+    num_of_cols = len(new_lists[0])
+    columns =[]
+    for i in range(num_of_cols):
+        col_i = []
+        for row in new_lists:
+            col_i.append(row[i])
+        columns.append(col_i)
+        print('column {0} is {1}'.format(i, columns[i]))
+    print('finished!')
